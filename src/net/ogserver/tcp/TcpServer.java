2d8065666426b55b75fcd6f8d60344b9f2c0c888
Christian <Christian@Chris.attlocal.net>
2015-02-11 20:03:10 -0600

    Added licensing

--- src/net/ogserver/tcp/TcpServer.java
+++ src/net/ogserver/tcp/TcpServer.java
@@ -1,5 +1,36 @@
 package net.ogserver.tcp;
 
+/*
+* Copyright (c) 2015
+* Christian Tucker.  All rights reserved.
+*
+* The use of OGServer is free of charge for personal use. 
+*
+* Commercial users are required to purchase a commercial license from
+* the OGServer website at http://ogserver.net/
+*
+* Commercial usage is defined by the amount of concurrent connections
+* that the server is handling at any given time. Once the server reaches
+* a state in which it is handling an average of 10 connections, your 
+* application is classified as 'Commercial'.
+*
+* Personal usage is only condoned if the following conditions are met:
+*
+* 1. It is required to mention the use of OGServer in your project, either
+*	 Through an opening or closing splash-screen lasting a minimum 3 seconds.
+*    This 'screen' is provided in the OGServer package.
+*
+* THIS SOFTWARE IS PROVIDED 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+* BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+* PARTICULAR PURPOSE, OR NON-INFRINGEMENT, ARE DISCLAIMED.  
+* IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+* THE POSSIBILITY OF SUCH DAMAGE.
+*/
 public class TcpServer {
 
 }
