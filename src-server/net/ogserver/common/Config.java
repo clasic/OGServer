afe178033a9bf67eb627cfe3efedf3cbb9f2e742
Christian <Christian@Chris>
2015-03-01 20:22:31 -0600

    Documentation

--- src-server/net/ogserver/common/Config.java
+++ src-server/net/ogserver/common/Config.java
@@ -2,6 +2,12 @@ package net.ogserver.common;
 
 public class Config {
 
+	/**
+	 * This value is used to tell the server rather or not the client is
+	 * prepared to accept a {@link Session#getSessionKey()} during the initial
+	 * connection phase, without this the linkage between a UDP packet and a {@link Session}
+	 * will not be possible.
+	 */
 	public static boolean enableUDP = false;
 	
 }
